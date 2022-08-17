3edd8bc8abc8f2ad5d72f95797bcb973f5c6b241
mcsavvy <phishink5@gmail.com>
2022-08-17 13:31:18 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ A task has a few attributes
 * name - name of the given task
 * worth - the number of points this task is worth
 * mandatory - is the task mandatory? True/False
-* earned - the number of points earned
-* fininshed_before_first_deadline - was the task completed before the first deadline? True/False
+* score - the number of points earned
+* before_first - was the task completed before the first deadline? True/False
+* before_second - was the task completed before the second deadline? True/False
 
+NOTE: If before_false and before_second is False, this program assumes that the task was completed after the second deadline
 """
 
 
@@ -20,90 +22,84 @@ from itertools import chain
 class task:
 	name: str
 	worth: int
-	mandatory: bool
-	earned: int
+	score: int
+	mandatory: bool = True
 	before_first: bool = False
 	before_second: bool = False
 	
+
+@dataclass
+class task_set:
+	all: "list[task]"
+	
+	@property
+	def before_first(self) -> "tuple[task]":
+		if not hasattr(self, "_bf"):
+			l = []
+			for task in self.all:
+				if task.before_first:
+					l.append(task)
+			self._bf = tuple(l)
+		return self._bf
+		
+	@property
+	def before_second(self) -> "tuple[task]":
+		if not hasattr(self, "_bs"):
+			l = []
+			for task in self.all:
+				if task.before_second:
+					l.append(task)
+			self._bs = tuple(l)
+		return self._bs
+		
+	@property
+	def after_second(self) -> "tuple[task]":
+		if not hasattr(self, "_as"):
+			l = []
+			for task in self.all:
+				if not (self.before_first or self.before_second):
+					l.append(task)
+			self._as = tuple(l)
+		return self._as
+		
+	@property
+	def worth(self) -> int:
+		return sum(map(attrgetter('worth'), self.all))
+		
+	@property
+	def before_first_score(self) -> float:
+		return sum(map(attrgetter("score"), self.before_first))
+		
+	@property
+	def before_second_score(self) -> float:
+		return sum(map(attrgetter("score"), self.before_second))
+
+	@property	
+	def after_second_score(self) -> float:
+		return sum(map(attrgetter("score"), self.after_second))
+		
+	@property
+	def score(self) -> float:
+		return (self.before_first_score / self.worth) + (
+			   (self.before_second_score / self.worth) * 0.65) + (
+			   (self.after_second_score / self.worth) * 0.5)
+			
+
 def score(tasks: "list[task]"):
 	# separate the tasks
-	mandatory_tasks: "dict[str, list[task]]" = {
-		"all": [],
-		"before_first": [],
-		"before_second": [],
-		"after_second": []
-	}
-	optional_tasks: "dict[str, list[task]]" = {
-		"all": [],
-		"before_first": [],
-		"before_second": [],
-		"after_second": []
-	}
-	M = mandatory_tasks
-	O = optional_tasks
-	for task in tasks:
-		if task.mandatory:
-			M['all'].append(task)
-			if task.before_first:
-				M['before_first'].append(task)
-			elif task.before_second:
-				M['before_second'].append(task)
-			else:
-				M['after_second'].append(task)
-		else:
-			O['all'].append(task)
-			if task.before_first:
-				O['before_first'].append(task)
-			elif task.before_second:
-				O['before_second'].append(task)
-			else:
-				O['after_second'].append(task)
-	M.update({
-		"total": sum(
- 			map(attrgetter("worth"), M['all'])
-		),
-		"before_first_deadline_total": sum(
-			map(attrgetter("earned"), M['before_first'])
-		),
-		"before_second_deadline_total": sum(
-			map(attrgetter("earned"), M['before_second'])
-		),
-		"after_second_deadline_total": sum(
-			map(attrgetter("earned"), M['after_second'])
-		)
-	})
-	M["score"] = (M['before_first_deadline_total'] / M['total']) + (
-		(M['before_second_deadline_total'] / M['total']) * 0.65) + (
-		(M['after_second_deadline_total'] / M['total']) * 0.50)
-	O.update({
-		"total": sum(
- 			map(attrgetter("worth"), O['all'])
-		),
-		"before_first_deadline_total": sum(
-			map(attrgetter("earned"), O['before_first'])
-		),
-		"before_second_deadline_total": sum(
-			map(attrgetter("earned"), O['before_second'])
-		),
-		"after_second_deadline_total": sum(
-			map(attrgetter("earned"), O['after_second'])
-		)
-	})
-	O["score"] = (O['before_first_deadline_total'] / O['total']) + (
-		(O['before_second_deadline_total'] / O['total']) * 0.65) + (
-		(O['after_second_deadline_total'] / O['total']) * 0.50)
-	
-	total = M["score"] + (M["score"] * O["score"])
+	mandatory_tasks = task_set([task for task in tasks if task.mandatory])
+	optional_tasks = task_set([task for task in tasks if not task.mandatory])
+	total = mandatory_tasks.score + (mandatory_tasks.score * optional_tasks.score)
 	print(f"Out Of {len(tasks)} task[s] completed:")
-	print(f"* {len(M['before_first'])} mandatory task[s] were completed before the first deadline")
-	print(f"* {len(M['before_second'])} mandatory task[s] were completed before the second deadline")
-	print(f"* {len(M['after_second'])} mandatory task[s] were completed after the second deadline")
-	print(f"* {len(O['before_first'])} optional task[s] were completed before the first deadline")
-	print(f"* {len(O['before_second'])} optional task[s] were completed before the second deadline")
-	print(f"* {len(O['after_second'])} optional task[s] were completed after the second deadline")
+	print(f"* {len(mandatory_tasks.before_first)} mandatory task[s] were completed before the first deadline")
+	print(f"* {len(mandatory_tasks.before_second)} mandatory task[s] were completed before the second deadline")
+	print(f"* {len(mandatory_tasks.after_second)} mandatory task[s] were completed after the second deadline")
+	print(f"* {len(optional_tasks.before_first)} optional task[s] were completed before the first deadline")
+	print(f"* {len(optional_tasks.before_second)} optional task[s] were completed before the second deadline")
+	print(f"* {len(optional_tasks.after_second)} optional task[s] were completed after the second deadline")
 	print('-' * 20)
-	print(f"+ IN MANDATORY TASKS, YOU SCORED {M['score']*100:.2f}% of {M['total']}")
-	print(f"+ IN OPTIONAL TASKS, YOU SCORED {O['score']*100:.2f}% of {O['total']}")
+	print(f"+ IN MANDATORY TASKS, YOU SCORED {mandatory_tasks.score*100:.2f}% of {mandatory_tasks.worth}")
+	print(f"+ IN OPTIONAL TASKS, YOU SCORED {optional_tasks.score*100:.2f}% of {optional_tasks.worth}")
 	print(f"+ TOTAL SCORE: {total*100:10.2f}%")
 	
 			
